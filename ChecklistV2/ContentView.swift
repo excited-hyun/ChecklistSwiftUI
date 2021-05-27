dfd5ac38056eb07ef9c54f2bbbba2ef07b0a21f7
excited-hyun <nahyun1127@naver.com>
2021-05-27 15:33:13 +0900

    Add navigationView

--- ChecklistV2/ContentView.swift
+++ ChecklistV2/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List {
-            Text("Walk the dog")
-            Text("Brush my teeth")
-            Text("Learn iOS development")
-            Text("Soccer practice")
-            Text("Eat ice cream")
-        }//End of list
+        NavigationView{
+            List {
+                Text("Walk the dog")
+                Text("Brush my teeth")
+                Text("Learn iOS development")
+                Text("Soccer practice")
+                Text("Eat ice cream")
+            }//End of list
+            .navigationBarTitle("CheckList")
+        }// End of navigationview
     }//end of body
 }
 
