82ce94a770d5a8dc1e561f14b7ffb04da6543728
excited-hyun <nahyun1127@naver.com>
2021-06-03 17:30:28 +0900

    make a checklist.swift for a ViewModel

---
+++ ChecklistV2/Checklist.swift
@@ -0,0 +1,34 @@
+//
+//  Checklist.swift
+//  ChecklistV2
+//
+//  Created by 신나현 on 2021/06/03.
+//
+
+import Foundation
+
+class Checklist: ObservableObject {
+    @Published var checklistItems = [
+        ChecklistItem(name: "Walk the dog", isChecked: false),
+        ChecklistItem(name: "Brush my teeth", isChecked: false),
+        ChecklistItem(name: "Learn iOS development", isChecked: true),
+        ChecklistItem(name: "Soccer practice", isChecked: false),
+        ChecklistItem(name: "Eat ice cream", isChecked: true),
+    ]
+
+    func printChecklistContent(){
+        for item in checklistItems {
+            print(item)
+        }
+    }
+    
+    func deleteListItem(whichElement: IndexSet) {
+        checklistItems.remove(atOffsets: whichElement)
+        printChecklistContent()
+    }
+    
+    func moveListItem(whichElement: IndexSet, destination: Int) {
+        checklistItems.move(fromOffsets: whichElement, toOffset: destination)
+        printChecklistContent()
+    }
+}
