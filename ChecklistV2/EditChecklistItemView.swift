2d3d5846694953dd5c3cfa6ef186891844db79b1
excited-hyun <nahyun1127@naver.com>
2021-06-03 17:49:15 +0900

    Change the start scene

---
+++ ChecklistV2/EditChecklistItemView.swift
@@ -0,0 +1,20 @@
+//
+//  EditChecklistItemView.swift
+//  ChecklistV2
+//
+//  Created by 신나현 on 2021/06/03.
+//
+
+import SwiftUI
+
+struct EditChecklistItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditChecklistItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditChecklistItemView()
+    }
+}
